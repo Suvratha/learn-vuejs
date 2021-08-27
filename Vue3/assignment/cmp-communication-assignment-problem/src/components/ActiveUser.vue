3ec5a62e057f3778265092c7342fabbe3796d1cc
Suvratha N <n.suvratha@gmail.com>
2021-08-27 22:06:22 +0200

    Assignment vue components
    wq

---
+++ Vue3/assignment/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label>Age:</label>
+      <input type="text" />
+    </div>
+    <button>Submit</button>
+  </form>
+</template>
+<script>
+export default {
+    props:{
+        name:{
+            type: String,
+            required: true
+        },
+        age:{
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
---
+++ Vue3/assignment/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <h2>Username: {{ username }}</h2>
+  </li>
+  <li>Age: {{ age }}</li>
+</template>
+<script>
+export default {
+  emits: ["new-user"],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("new-user", this.enteredName, this.enteredAge);
+    },
+  },
+};
+</script>
